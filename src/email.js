82091da3800e84a402501b956eefcf97531e8fad
Andrzej Dybionka <andrzej@arabel.la>
2017-05-17 14:18:00 +0200

    USA zipcode validator

--- src/email.js
+++ src/email.js
@@ -1,5 +1,7 @@
+const regexp = !/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
 export const email = (value) => {
-  if (value && (!/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i.test(value))) {
+  if (value && (regexp.test(value))) {
     return 'Email format invalid';
   }
 
