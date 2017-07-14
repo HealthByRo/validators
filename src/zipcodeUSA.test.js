7154c8582b7c6c46d1db9a3ad24ace78f7a41d24
Andrzej Dybionka <andrzej@arabel.la>
2017-07-14 12:23:36 +0200

    Code review

--- src/zipcodeUSA.test.js
+++ src/zipcodeUSA.test.js
@@ -7,27 +7,31 @@ describe('zipcodeUSA', () => {
     expect(zipcodeUSA).toBeDefined();
   });
 
-  it('should return error message when zip code has less then 4 digits', () => {
-    expect(zipcodeUSA('123')).toEqual(errorMessage);
+  it('should return error message when zip code has less then 5 digits', () => {
+    expect(zipcodeUSA('1234')).toEqual(errorMessage);
   });
 
   it('should return error message when zip code has more then 5 digits', () => {
     expect(zipcodeUSA('123456')).toEqual(errorMessage);
   });
 
-  it('should return error message when zip code has more then 4 chars but not all of them are digits', () => {
-    expect(zipcodeUSA('12ab')).toEqual(errorMessage);
+  it('should return error message when zip code has 5 characters but not all of them are digits', () => {
+    expect(zipcodeUSA('123ab')).toEqual(errorMessage);
   });
 
-  it('should return error message when zip code has more then 6 chars but not all of them are digits', () => {
-    expect(zipcodeUSA('1234a')).toEqual(errorMessage);
+  it('should return error message when zip code has 5 characters but not all of them are digits', () => {
+    expect(zipcodeUSA('123AB')).toEqual(errorMessage);
   });
 
-  it('should not return error message when zip code has 4 digits', () => {
+  it('should not return error message when zip code has 5 digits', () => {
     expect(zipcodeUSA('12345')).toBeUndefined();
   });
 
   it('should not return error message when zip code has 5 and 4 digits', () => {
     expect(zipcodeUSA('12345-1234')).toBeUndefined();
+  });
+
+  it('should return error message when zip code has 5 and 5 digits', () => {
+    expect(zipcodeUSA('12345-12345')).toEqual(errorMessage);
   });
 });
