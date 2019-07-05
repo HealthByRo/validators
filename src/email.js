9a1e587f0c4393e7d3b9e34e5fc5239703279058
Mariusz Jarząbek <mariusz@ro.co>
2019-07-05 14:55:15 +0200

    change email validator

--- src/email.js
+++ src/email.js
@@ -1,17 +1,8 @@
 import { EMAIL_ERROR_MSG } from './constants';
-
-export const matchRegexp = (regexp, errorMsg) => (value) => {
-  const email = value.trim();
-
-  if (email && !regexp.test(email)) {
-    return errorMsg;
-  }
-
-  return undefined;
-};
+import matchRegexp from './matchRegexp';
 
 export const EMAIL_REGEXP = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
 
-export const email = matchRegexp(EMAIL_REGEXP, EMAIL_ERROR_MSG);
+export const email = (value) => matchRegexp(EMAIL_REGEXP, EMAIL_ERROR_MSG)(value.trim());
 
 export default email;
