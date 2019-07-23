2ab253cc56d4eb42b3b592c21cacaf1b7bc4b8b7
Mariusz Jarząbek <mariusz@ro.co>
2019-07-23 11:48:47 +0200

    change email validator

--- src/email.js
+++ src/email.js
@@ -3,6 +3,6 @@ import matchRegexp from './matchRegexp';
 
 export const EMAIL_REGEXP = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
 
-export const email = (value) => matchRegexp(EMAIL_REGEXP, EMAIL_ERROR_MSG)(value.trim());
+export const email = (value) => matchRegexp(EMAIL_REGEXP, EMAIL_ERROR_MSG)(value ? value.trim() : undefined);
 
 export default email;
